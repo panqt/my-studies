6b4150981b9e69fcfa467d241197f861c1407dc4
panqt <1255457995@qq.com>
2019-04-07 06:49:17 +0800

    测试dubbo微服务故障转移，通过

--- dubbo/dubbo-customer/src/test/java/pers/panqt/dubbo/customer/DubboCustomerApplicationTests.java
+++ dubbo/dubbo-customer/src/test/java/pers/panqt/dubbo/customer/DubboCustomerApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pers.panqt.dubbo.provider.api.UserService;
 import pers.panqt.dubbo.provider.entry.User;
 
@@ -17,8 +19,13 @@ public class DubboCustomerApplicationTests {
 
     @Test
     public void contextLoads() {
-        User user = service.findById(1);
-        System.out.println(user);
+        int i = 0;
+        while (true){
+            i++;
+            User user = service.findById(i);
+            System.out.println(user);
+        }
+
     }
 
 }
